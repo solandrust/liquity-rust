73896fefe3f79fe4c3a6ce5224931acfb50f1624
solandrust <zhanghe.1534@gmail.com>
2021-08-08 19:44:45 -0600

    Add basic anchor

--- packages/contracts/contracts/liquity-base/src/lib.rs
+++ packages/contracts/contracts/liquity-base/src/lib.rs
@@ -1,14 +1,51 @@
-use solana_program::entrypoint;
-use solana_program::pubkey::Pubkey;
-use solana_program::account_info::AccountInfo;
-use solana_program::entrypoint::ProgramResult;
+use anchor_lang::prelude::*;
 
-entrypoint!(process_instruction);
+#[program]
+mod counter {
+    use super::*;
 
-pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    geonft_data: &[u8],
-) -> ProgramResult {
-    panic!()
+    pub fn initialize(ctx: Context<Initialize>,
+                      authority: Pubkey) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        counter.authority = authority;
+        counter.count = 0;
+
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        counter.count += 1;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init)]
+    pub counter: ProgramAccount<'info, Counter>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: ProgramAccount<'info, Counter>,
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("You are not authorized to perform this action")]
+    Unauthorized,
 }
